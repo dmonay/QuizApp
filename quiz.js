0b2d0d59cf75a7c31582b88cd6bc41ead015edd8
Dmitry Pavluk <pavluk73@gmail.com>
2014-02-11 11:02:59 -0500

    v0.81

--- quiz.js
+++ quiz.js
@@ -4,50 +4,8 @@
 var counter = 0;
 
    
-
-
-//QUESTIONS
-var questionArray = [];
-questionArray[0] = "What is a bourbon?";
-questionArray[1] = "What is peat?";
-questionArray[2] = "What does unchillfiltered mean?";
-
-
-    
-    
-    
-
-
-
-
-//ANSWERS
-
-var answerArray = [];
-answerArray[0] = ["A whiskey made on planet Earth", "A whiskey made in the U.S.", "A whiskey made in Kentucky", "A nebulous legal term"];
-answerArray[1] = ["a", "b", "c", "d"];
-answerArray[2] = ["aa", "bb", "cc", "dd"];
-                     
-        
-        
-        
-        
-        
-        
-//EXPLANATIONS
-        
-var explanationArray = [];
-explanationArray[0] = "A bourbon is....";
-explanationArray[1] = "Peat is....";
-explanationArray[2] = "For a whiskey to be....";
-
 $(document).ready(function () {
-    
-/*    function FullQuestion(question, answerArray, explanation) {
-        this.question = question;
-        this.answerArray = answerArray;
-        this.explanation = explanation;
-    }
- */    
+  
     
 
     //convert array of answers into list elements:
